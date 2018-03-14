44ffc60853bfad55194dc601c440eb3c3873beb6
Helmut Dobretzberger <helmut.dobretzberger@active-group.de>
2018-03-14 13:23:09 +0100

    Add a first transformTest to test the latest bug

---
+++ src/test/scala/de/activegroup/scalajasper/core/TransformTests.scala
@@ -0,0 +1,27 @@
+package de.activegroup.scalajasper.core
+
+import de.activegroup.scalajasper.core.components.{TableCell, TableGroupCell}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+@RunWith(classOf[JUnitRunner])
+class TransformTests extends FunSuite {
+
+  test("table group cell is set appropiate") {
+    val initTransform = TransformationState.initial(0 px)
+
+    // use rowSpan just a an idication that the cells are equal.
+    // Why are not equals-methods implemented in jasper reports?
+    val tc = TableCell(content = Vector(), rowSpan = Some(723))
+    val groupName = "group1"
+
+    val tgc = TableGroupCell(groupName, tc)
+
+    val tgcTransformed = tgc.transform.exec(initTransform)._1
+    val tcTransformed = tc.transform.exec(initTransform)._1
+
+    assert(tgcTransformed.getGroupName == groupName)
+    assert(tgcTransformed.getCell.getRowSpan == tcTransformed.getRowSpan)
+
+  }
+}
